3c20bb66f2c884d96fde7ca561b37fa727d9fe66
ivanjeremic <ivan.jeremic@outlook.com>
2022-03-14 14:17:29 +0100

    db api

--- core/plugins/core-db-engine.js
+++ core/plugins/core-db-engine.js
@@ -2,7 +2,40 @@ import fs from "fs/promises";
 import { open } from "lmdb";
 
 export default async function (db, opts, next) {
-  db.get("/www", (request, reply) => {
-    reply.send({ www: "www" });
+  // get list of all databases in current cluster????
+  db.get("/admindb/databases", async (request, reply) => {
+    const dbs = await fs.readdir("databases");
+
+    reply.send({ data: request.query.databases });
+  });
+
+  // some other endpoint
+  db.get("/admindb", async (request, reply) => {
+    let myDB = open({
+      path: `databases/admindb`,
+      // any options go here, we can turn on compression like this:
+      compression: true,
+    });
+
+    await myDB.put("admindb", {
+      clusters: [{ name: "c1", uri: "https://foo/api", databases: [] }],
+    });
+
+    const res = myDB.get("admindb");
+
+    reply.send({ data: res });
+  });
+
+  // test database
+  db.get("/www", async (request, reply) => {
+    let myDB = open({
+      path: `databases/my-db`,
+      // any options go here, we can turn on compression like this:
+      compression: true,
+    });
+
+    await myDB.put("greeting", { someText: "Hello, World!" });
+
+    reply.send({ data: "" });
   });
 }
