d465e4fc6eeb3bdbfebf47d8713c58ea2fecbce4
ivanjeremic <ivan.jeremic@outlook.com>
2023-03-18 14:07:51 +0100

    bre

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello WORKER!</h1>");
+  res.send("<h1>Hello Bre!</h1>");
 });
 
 // start the server listening for requests
