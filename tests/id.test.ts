c69afa59c64db2d568e8a8ff07c4d9fc769576e4
ivanjeremic <ivan.jeremic@outlook.com>
2024-10-05 20:42:58 +0200

    test

--- tests/id.test.ts
+++ tests/id.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "bun:test";
 import { genIdV001, parseId } from "../packages/utils/src";
 
+// test if id generator fn 'genIdV001()' is generating an id with version 001
 test(genIdV001(), () => {
   expect(genIdV001().slice(0, 3)).toBe("001");
 });
