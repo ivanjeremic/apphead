a85715280a8c39605d4eb698f90ad947640c1a5a
ivanjeremic <ivan.jeremic@outlook.com>
2025-04-10 06:28:11 +0200

    work on ui

--- packages/my-remix-app/public/build/_shared/react-PCUW7WH5.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react
-} from "/build/_shared/chunk-L5UUKKP6.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export default require_react();
-//# sourceMappingURL=/build/_shared/react-PCUW7WH5.js.map
--- packages/my-remix-app/public/build/_shared/react-dom-5MSMPZNK.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "/build/_shared/chunk-3OTT62YY.js";
-import "/build/_shared/chunk-L5UUKKP6.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export default require_react_dom();
-//# sourceMappingURL=/build/_shared/react-dom-5MSMPZNK.js.map
